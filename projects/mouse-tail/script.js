6387920d223940823aa26460c56dc344b73dbf7a
AKregel <akregel23@gmail.com>
2016-10-11 21:09:01 +0200

    Update script.js

--- projects/mouse-tail/script.js
+++ projects/mouse-tail/script.js
@@ -1,3 +1,17 @@
+var display = document.getElementById('display');
+display.onmousemove = handler;
+var segments = [];
+
+function handler(event){
+	var x = event.offsetX;
+	var y = event.offsetY;
+	var newSegment = createElement(x, y);
+	segments.unshift(newSegment)
+	if (segments.length > 15){
+		removeSegment(segments.pop());
+	}
+	registerDie(newSegment);
+}
 //
 function createSegment(x, y){
 	var displayObject = document.getElementById("display");
