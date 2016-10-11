6504604f27bf1b6edfdd9972f552adbd68342b7d
2l8 <dashukevich1995@gmail.com>
2016-10-11 21:46:53 +0300

    Update script.js

--- projects/mouse-tail/script.js
+++ projects/mouse-tail/script.js
@@ -1,1 +1,10 @@
 //
+function createSegment(x, y){
+	var displayObject = document.getElementById("display");
+	var segment = document.createElement("div");
+	segment.classList.add("segment");
+	segment.style.left = x;
+	segment.style.top = y;
+	displayObject.appendChild(segment);
+	return segment;
+}
