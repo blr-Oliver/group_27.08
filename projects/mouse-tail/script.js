742365cc1e606d90ed80eeb13e9d213b87c0679e
Vasily Liaskovsky <vasily.liaskovsky@gmail.com>
2016-10-11 22:17:30 +0300

    Update script.js

--- projects/mouse-tail/script.js
+++ projects/mouse-tail/script.js
@@ -5,7 +5,7 @@ var segments = [];
 function handler(event){
 	var x = event.offsetX;
 	var y = event.offsetY;
-	var newSegment = createElement(x, y);
+	var newSegment = createSegment(x, y);
 	segments.unshift(newSegment)
 	if (segments.length > 15){
 		removeSegment(segments.pop());
@@ -14,23 +14,22 @@ function handler(event){
 }
 //
 function createSegment(x, y){
-	var displayObject = document.getElementById("display");
 	var segment = document.createElement("div");
 	segment.classList.add("segment");
 	segment.style.left = x + "px";
 	segment.style.top = y + "px";
-	displayObject.appendChild(segment);
+	display.appendChild(segment);
 	return segment;
 }
 
 function registerDie (segment) {
-	setTimeout(removeSegment, 1000, segment);
+	setTimeout(removeSegment, 300, segment);
 }
 
 function removeSegment (segment) {
+	segment.remove();
 	var index = segments.indexOf(segment);
 	if(index != -1){
-		segment.remove();
 		segments.splice(index, 1);
 	}
 }
