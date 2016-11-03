dcbb6e0b5c87348f7d0d472c785c7eb7257fa783
Mcihael Fursa <bellamy9678@yahoo.com>
2016-11-03 21:01:13 +0200

    Update script.js

--- projects/time-table/js/script.js
+++ projects/time-table/js/script.js
@@ -1,11 +1,11 @@
 angular.module('timeTable',[]).controller('TimeTableController', function ($scope) {
 	$scope.currentTime = 100;
 	$scope.tableContent = [{
-	type: 	'T',
-	number: '53',
-	route: 	'зеленый луг',
-	time1:	5,
-	time2:	15	
+		type: 	'T',
+		number: '53',
+		route: 	'зеленый луг',
+		time1:	5,
+		time2:	15	
 	}];
 	$scope.refresh = function () {
 		
