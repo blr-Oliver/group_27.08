d75b19a887502f3f4772bb0fd45a67fecc07f890
Alexander <dashukevich1995@gmail.com>
2016-11-10 20:34:34 +0300

    Update pagination.js

--- projects/pagination/js/pagination.js
+++ projects/pagination/js/pagination.js
@@ -57,5 +57,31 @@ angular.module("pagination", []).component("pagination",
 				disabled: false
 			}
 		];
-	}]
+	},
+	     this.createPages = function(){
+		var completedArray = [];
+		     completedArray.push(this.createPage("arrow", -1, false, this.state.current == 1));
+		     if (this.state.current - 1 < 4){
+			 for(var i = 1; i <= 3; i++){
+			     completedArray.push(this.createPage("number", i, false, false));
+			 }
+		     }else{
+			 completedArray.push(this.createPage("number", 1, false, false));
+			 completedArray.push(this.createPage("dots", null, false, true));
+			 completedArray.push(this.createPage("number", this.state.current - 1, false, false));
+		     }
+		     completedArray.push(this.createPage("number", this.state.current, true, false));
+		     if (this.state.last - this.state.current < 4){
+			 for(var j = this.state.current + 1; j <= this.state.last; j++){
+			     completedArray.push(this.createPage("number", j, false, false));
+			 }
+		     }else{
+			 completedArray.push(this.createPage("number", this.state.current + 1, false, false));
+			 completedArray.push(this.createPage("dots", null, false, true));
+			 completedArray.push(this.createPage("number", this.state.last, false, false));
+		     }
+		     completedArray.push(this.createPage("arrow", 1, false, this.state.current == this.state.last));
+		     return completedArray;
+    	}
+	]
 });
