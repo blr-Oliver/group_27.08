578a404e2f13419c14b28627199881c93db5f532
Mcihael Fursa <bellamy9678@yahoo.com>
2016-11-17 21:03:55 +0200

    Add files via upload

---
+++ projects/AngularSVG/js/angular-svg-graph.js
@@ -0,0 +1,22 @@
+angular.module("angularSVGGraph", []).directive("linearGraph",
+  [function(){
+    return {
+      templateUrl: "/linear-graph.html",
+      controller: ["$scope", function($scope){}],
+      scope: {
+        scale: "=?",
+        xArray: "=?",
+        yArray: "=?"
+      },
+      restrict: "E",
+      controllerAs: "graph",
+      bindToController: false,
+      link: function ($scope, $element, $attrs){
+        $scope.origin = {
+          x: 30,
+          y: 60
+        }
+      }
+    }
+  }])
+  
