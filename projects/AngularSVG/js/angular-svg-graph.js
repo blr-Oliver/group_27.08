aba536caf88307ebac91fa014e571a765e05e81d
Vasily Liaskovsky <vasily.liaskovsky@gmail.com>
2016-11-20 11:24:13 +0200

    more dynamic SVG + some styling

--- projects/AngularSVG/js/angular-svg-graph.js
+++ projects/AngularSVG/js/angular-svg-graph.js
@@ -1,22 +1,25 @@
-angular.module("angularSVGGraph", []).directive("linearGraph",
-  [function(){
-    return {
-      templateUrl: "/linear-graph.html",
-      controller: ["$scope", function($scope){}],
-      scope: {
-        scale: "=?",
-        xArray: "=?",
-        yArray: "=?"
-      },
-      restrict: "E",
-      controllerAs: "graph",
-      bindToController: false,
-      link: function ($scope, $element, $attrs){
-        $scope.origin = {
-          x: 30,
-          y: 60
-        }
-      }
+angular.module("angularSVGGraph", []).directive("linearGraph", [function(){
+  return {
+    templateUrl: "/linear-graph.html",
+    controller: ["$scope", function($scope){
+    }],
+    scope: {
+      scale: "=?",
+      xArray: "=?",
+      yArray: "=?"
+    },
+    restrict: "E",
+    controllerAs: "graph",
+    bindToController: false,
+    link: function($scope, $element, $attrs){
+      $scope.origin = {
+        x: 30,
+        y: 60
+      };
+      $scope.unit = {
+        x: 10,
+        y: 10
+      };
     }
-  }])
-  
+  }
+}])
