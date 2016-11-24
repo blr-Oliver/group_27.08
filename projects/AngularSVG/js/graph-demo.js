febbeb3280a41ce698ecfb2dabad745bd11facf8
AKregel <akregel23@gmail.com>
2016-11-24 22:15:12 +0300

    Update graph-demo.js

--- projects/AngularSVG/js/graph-demo.js
+++ projects/AngularSVG/js/graph-demo.js
@@ -2,19 +2,9 @@ angular.module('graphDemo', ['svgGraph']).controller('GraphDemoController', ['$s
   // y = 2*(x - 0.5)^2 - 1
   $scope.xData = [-1.5, -1.0, -0.5, +0.0, +0.5, +1.0, +1.5];
   $scope.yData = [+7.0, +3.5, +1.0, -0.5, -1.0, -0.5, +1.0];
+  $scope.xData1 = [-1, 1];
+  $scope.yData1 = [-1, 1];
 
-  $scope.series = {
-  	default: { 
-  		x: $scope.xData,
-  		y: $scope.yData
-  	},
-  	series: {
-  		x: [-1,10],
-  		y: [-1,10]
-  	}
-  }
-console.log(JSON.stringify($scope.series));
-  
   $scope.data = $scope.xData.map(function(e, i){
     return {
       x: e,
