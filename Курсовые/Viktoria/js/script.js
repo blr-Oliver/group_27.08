6bfb7a5028b9c351ce166b449961acc3aecdffda
Vasily Liaskovsky <vasily.liaskovsky@gmail.com>
2016-11-25 22:40:37 +0200

    удален ненужный и повторяющийся код

--- Курсовые/Viktoria/js/script.js
+++ Курсовые/Viktoria/js/script.js
@@ -1,74 +1,43 @@
-number = 0 
-var imagesSlider = [new Image(), new Image(), new Image(), new Image()];
-imagesSlider[0].src="http://otdyhaem.by/upload/medialibrary/ef3/ef34a12a95dcd424935feba2336943e0.jpg"; // Riga
-imagesSlider[1].src="http://respector.by/wp-content/uploads/2016/03/01.jpg"; // Tallin
-imagesSlider[2].src="http://www.softtour.by/img/tours/31/Vilnius.jpeg"; // Vilnius
-imagesSlider[3].src="http://img.tyt.by/n/fotofact/09/e/18minsk-foto-dmitriy_vazhnik-yanvar2016.jpg"; // Minsk
-setInterval(function sliderPictures(){
-    $("#image")[0].src = imagesSlider[number].src; 
-    number++; 
-    if(number === imagesSlider.length){
-        number = 0;
-    };
-}, 2500);
+var links = [
+  "http://otdyhaem.by/upload/medialibrary/ef3/ef34a12a95dcd424935feba2336943e0.jpg", // Riga
+  "http://respector.by/wp-content/uploads/2016/03/01.jpg", //Tallin
+  "http://www.softtour.by/img/tours/31/Vilnius.jpeg", // Vilnius
+  "http://img.tyt.by/n/fotofact/09/e/18minsk-foto-dmitriy_vazhnik-yanvar2016.jpg" // Minsk
+];
 
-function showPicture1(){
-    $("#image")[0].src=imagesSlider[0].src; 
-}
-function showPicture2(){
-    $("#image")[0].src=imagesSlider[1].src; 
-}
-function showPicture3(){
-    $("#image")[0].src=imagesSlider[2].src; 
-}
-function showPicture4(){
-    $("#image")[0].src=imagesSlider[3].src; 
-}
+$(document).ready(function(){
+  var images = preload(links);
 
-$(function(){
-    var flag = false;
-    $("#link1").css("background", "#000");
-    setTimeout(function () {
-        $("#link1").css("background", "#000");
-        setInterval(function () {
-            $("#link1").css("background", flag ? "#000" : "#bd3333");
-            flag = !flag;
-        }, 500);
-    }, 3000);
+  $('.map').click(function(event){
+    var target = $(event.target);
+    if(target.is('a.spot'))
+      showImage(target.index());
+  });
+  
+  setTimeout(blinkInfinitely, 3000, '.spot', 'highlight', 500);
+  setTimeout(rotateInfinitely, 2500, 0);
 });
 
-$(function(){
-    var flag = false;
-    $("#link2").css("background", "#000");
-    setTimeout(function () {
-        $("#link2").css("background", "#000");
-        setInterval(function () {
-            $("#link2").css("background", flag ? "#000" : "#bd3333");
-            flag = !flag;
-        }, 500);
-    }, 3000);
-});
+function preload(links){
+  return links.map(function(src){
+    var img = new Image();
+    img.src = src;
+    return img;
+  })
+}
 
-$(function(){
-    var flag = false;
-    $("#link3").css("background", "#000");
-    setTimeout(function () {
-        $("#link3").css("background", "#000");
-        setInterval(function () {
-            $("#link3").css("background", flag ? "#000" : "#bd3333");
-            flag = !flag;
-        }, 500);
-    }, 3000);
-});
+function showImage(index){
+  $("#image")[0].src = links[index];
+}
 
-$(function(){
-    var flag = false;
-    $("#link4").css("background", "#000");
-    setTimeout(function () {
-        $("#link4").css("background", "#000");
-        setInterval(function () {
-            $("#link4").css("background", flag ? "#000" : "#bd3333");
-            flag = !flag;
-        }, 500);
-    }, 3000);
-});
+function blinkInfinitely(selector, claz, interval){
+  var target = $(selector);
+  setInterval(function(){
+    target.toggleClass(claz);
+  }, interval);
+}
+
+function rotateInfinitely(toIndex){
+  showImage(toIndex);
+  setTimeout(rotateInfinitely, 2500, (toIndex + 1) % links.length);
+}
